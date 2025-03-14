c1ba92cee941583e44312b2a3c72f1d15172089a
Jeka_Robot <petryakov.evgeni@gmail.com>
2025-03-14 20:05:24 +0300

    replacement BrowserRouter with HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Results from '../pages/Results/Results'
 import '../shared/styles/global.css'
